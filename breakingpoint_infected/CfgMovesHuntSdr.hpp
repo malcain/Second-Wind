750e356bf58d09d9c55c3681ec6f77345463f985
Malcain <malcains@gmail.com>
2019-01-15 02:15:20 +0300

    Revert "Revert "hunt spd refix""
    This reverts commit 562a7f461bb9d8309857cf851f9942ed47247539.

---
+++ breakingpoint_infected/CfgMovesHuntSdr.hpp
@@ -0,0 +1,45 @@
+
+class CfgMovesMaleSdr;
+
+class CfgMovesMaleSdrHunt3 : CfgMovesMaleSdr
+{
+	class States
+	{
+		class AmovPercMstpSlowWrflDnon; //stay weap low
+		class AmovPercMstpSrasWrflDnon; //stay weap up
+		class AmovPknlMstpSlowWrflDnon; //kneel weap low
+		class AmovPknlMstpSrasWrflDnon; //kneel weap up
+		class AmovPsitMstpSrasWrflDnon; //sit weap
+		class AmovPercMstpSrasWrflDnon_Salute; //salute weap up
+		class AmovPercMstpSlowWrflDnon_Salute; //salute weap low
+		class AmovPercMstpSlowWpstDnon; //stay pistol low
+		class AmovPercMstpSrasWpstDnon; //stay pistol up
+		class AmovPknlMstpSlowWpstDnon; //kneel pistol low
+		class AmovPknlMstpSrasWpstDnon; //kneel pistol up
+		class amovpsitmstpsnonwpstdnon_ground; //sit pistol
+		class AmovPercMstpSrasWpstDnon_Salute; //salute pistol up
+		class AmovPercMstpSnonWnonDnon; //stay no weapon
+		class AmovPknlMstpSnonWnonDnon; //kneel no weapon
+		class AmovPpneMstpSnonWnonDnon; //prone no weapon
+		class AmovPsitMstpSnonWnonDnon_ground; //sit no weapon
+		class amovpercmstpsnonwnondnon_salute; //salute no weapon
+		class AmovPercMstpSsurWnonDnon; //Surrender no weapon
+		class AmovPercMstpSrasWlnrDnon; //stay launcher up
+		class AmovPercMstpSlowWlnrDnon; //stay launcher low
+		class AmovPknlMstpSrasWlnrDnon; //kneel launcher
+		//class AmovPknlMstpSnonWnonDf; 	//kneel no weapon forward
+		class AmovPercMstpSlowWrflDnon_gear;
+		class AovrPercMstpSnonWnonDf; //vault from object
+		//class AinvPknlMstpSnonWnonDnon_medic;
+
+		class AmovPercMrunSlowWrflDf
+		{
+			speed = 2.715882; //0.715882
+		};
+		class AmovPercMrunSrasWrflDf
+		{
+			speed = 2.684541; //0.684541
+		};
+	
+	};
+};
